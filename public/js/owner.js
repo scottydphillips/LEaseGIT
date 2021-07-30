63923c457e5bcab55fae7240dcc24db757c3af34
Pia Paulino <maripia1220@gmail.com>
2021-07-30 13:27:10 -0400

    Working on owner.js and owner route.js

---
+++ public/js/owner.js
@@ -0,0 +1,24 @@
+
+
+const ownerFormHandler = async (event) => {
+  event.preventDefault();
+
+  const propertyAddress = document.getElementById("property-address").value;
+  const leaseLength = document.getElementById("lease-length").value;
+
+  if (propertyAddress && leaseLength) {
+    const response = await fetch("/api/user/owner", {
+      method: "POST",
+      body: JSON.stringify({ propertyAddress, leaseLength }),
+      headers: { "content-type": "owner/json" },
+    });
+    console.log(response);
+    if(response.ok) {
+        document.location.replace('/owner');
+    } else {
+        alert(response.statusText);
+    }
+
+  }
+}; 
+    
