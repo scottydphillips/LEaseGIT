b13b5d8700e99e3ccdc8c1944d833b87b94bec64
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-02 18:14:20 -0400

    Furniture in room planner can now be created, removed and roatted with keyboard press events. These key controls displayed on right under Decoration Tools.

--- public/js/plan.js
+++ public/js/plan.js
@@ -1,6 +1,9 @@
 
-const houseWidth = window.innerWidth * 0.8;
-const houseHeight = window.innerHeight * 0.8;
+const stage = document.getElementById('stage'); //DOM element that room uses
+let roomWidth = window.innerWidth * 0.6;
+let roomHeight = window.innerHeight * 0.8;
+let targetFurniture;
+let copyReady = true;
 
 const loadImage = (imageUrl, whenLoaded) => {
     const img = new Image();
@@ -15,15 +18,15 @@ const loadImage = (imageUrl, whenLoaded) => {
   const addFloor = (imageUrl) => {
     loadImage(imageUrl,
       (image) => {
-        floor = Bodies.rectangle(houseWidth/2, houseHeight/2, 1, 1, {
+        floor = Bodies.rectangle(roomWidth/2, roomHeight/2, 1, 1, {
             density: 0.0005,
             frictionAir: 0.2,
             restitution: 0.3,
             render: {
               sprite: {
                 texture: image.src, 
-                xScale: (houseWidth/image.width),
-                yScale: (houseHeight/image.height)
+                xScale: (roomWidth/image.width),
+                yScale: (roomHeight/image.height)
               }
             },
             collisionFilter: {
@@ -78,7 +81,7 @@ const loadImage = (imageUrl, whenLoaded) => {
     });
   }
 
-    //Add floor and remove collision
+    //Add four-sided furniture
     const addRectFurniture = (x, y, width, height, imageUrl) => {
       loadImage(imageUrl,
         (image) => {
@@ -107,14 +110,15 @@ Runner = Matter.Runner,
 Composite = Matter.Composite,
 MouseConstraint = Matter.MouseConstraint,
 Mouse = Matter.Mouse;
+Events = Matter.Events;
     
 const engine = Engine.create();
 const render = Render.create({
-    element: document.getElementById('stage'),
+    element: stage,
     engine: engine,
     options: {
-        width: houseWidth,
-        height: houseHeight,
+        width: roomWidth,
+        height: roomHeight,
         wireframes: false,
         background: 'white'
     }
@@ -132,7 +136,7 @@ const bed2Url = "/images/furniture/bed2.png";
 const loveseatUrl = "/images/furniture/loveseat.png";
 const plushChairUrl = "/images/furniture/plush-chair.png";
 const tvStandUrl = "/images/furniture/tv-stand.png";
-const nightstandUrl = "./images/furniture/nightstand.png";
+const nightstandUrl = "/images/furniture/nightstand.png";
 const workspaceUrl = "/images/furniture/workspace.png";
 const diningRoomTableUrl = "/images/furniture/dining-room-table.png";
 const roundMarbleTableUrl = "/images/furniture/round-marble-table.png";
@@ -164,10 +168,10 @@ Render.run(render);
 //set brief delay to allow floor to load both its texture and body first
 setTimeout(function(){
   //walls
-  addWall(0, 0, houseWidth*2, 50, wallUrl);
-  addWall(0, 0, 50, houseHeight*2, wallUrl);
-  addWall(houseWidth, 0, 50, houseHeight*2, wallUrl);
-  addWall(0, houseHeight, houseWidth*2, 50, wallUrl);
+  addWall(0, 0, roomWidth*2, 50, wallUrl);
+  addWall(0, 0, 50, roomHeight*2, wallUrl);
+  addWall(roomWidth, 0, 50, roomHeight*2, wallUrl);
+  addWall(0, roomHeight, roomWidth*2, 50, wallUrl);
   //furniture
   addRectFurniture(80, 50, 250, 100, loveseatUrl);
   addRectFurniture(300, 50, 300, 110, sofaUrl);
@@ -185,3 +189,50 @@ function selectFloor() {
   let selectedFloor = document.getElementById("floor-picker").value;
   floor.render.sprite.texture = `/images/floors/${selectedFloor}.png`
 }
+
+//Capture body user is targeting
+Events.on(mouseConstraint, "startdrag", function(e){
+  targetFurniture = e.body;
+})
+
+//Keypress Events: Delete, Copy, Rotate
+window.addEventListener('keydown', (e) => {
+  //Delete furniture
+  if (e.key == 'Delete'|| e.key == 'Backspace' || e.key == 'r')
+    World.remove(engine.world, targetFurniture);
+
+  //Copy furniture
+  if (copyReady && (e.key == 'Insert' || e.key == 'c' || e.key == 'n')){
+    
+    if (targetFurniture.parent.label == "Circle Body"){
+      addRoundFurniture(roomWidth/2, roomHeight/2,
+        targetFurniture.circleRadius, targetFurniture.render.sprite.texture);
+    } else {
+      let newWidth = targetFurniture.vertices[1].x - targetFurniture.vertices[0].x;
+      let newLength = targetFurniture.vertices[2].y - targetFurniture.vertices[1].y;
+      addRectFurniture(roomWidth/2, roomHeight/2,
+                      newWidth, newLength, targetFurniture.render.sprite.texture);
+    }
+
+    //Brief delay before copying again
+    copyReady = false;
+    setTimeout(function(){
+      copyReady = true;
+    }, 200);
+  }
+
+  //Rotate Left
+  if (e.key == 'ArrowLeft' || e.key == 'a' || e.key == 'w')
+    Matter.Body.rotate(targetFurniture, -0.07);
+
+  //Rotate Right
+  if (e.key == 'ArrowRight' || e.key == 'd' || e.key == 's')
+    Matter.Body.rotate(targetFurniture, 0.07);
+  });
+
+// window.addEventListener('resize', () => {
+//   roomWidth = window.innerWidth * 0.6;
+//   roomHeight = window.innerHeight * 0.8;
+//   render.width = roomWidth;
+//   render.height = roomHeight;
+// });
