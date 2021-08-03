a4aff8f02b21466ede5dfd00f4ee639e12978312
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-02 20:20:24 -0400

    Put all furniture data in data objects. Added electric stove, fridge, double fridge, white chair, patio table with chairs, potted plant, marble countertop, glass and wood tables. Drop down menu allows to add any furniture. Made sample room a little more flexible to screen size. Removed stage margin.

--- public/js/plan.js
+++ public/js/plan.js
@@ -129,22 +129,37 @@ engine.gravity.y = 0;
 const placeholderFlooringUrl = "/images/floors/chevron-hardwood-floor.png"
 //wall path
 const wallUrl = "/images/floors/wall.png"
-//furniture paths
-const sofaUrl = "/images/furniture/sofa.png";
-const bed1Url = "/images/furniture/bed1.png";
-const bed2Url = "/images/furniture/bed2.png";
-const loveseatUrl = "/images/furniture/loveseat.png";
-const plushChairUrl = "/images/furniture/plush-chair.png";
-const tvStandUrl = "/images/furniture/tv-stand.png";
-const nightstandUrl = "/images/furniture/nightstand.png";
-const workspaceUrl = "/images/furniture/workspace.png";
-const diningRoomTableUrl = "/images/furniture/dining-room-table.png";
-const roundMarbleTableUrl = "/images/furniture/round-marble-table.png";
-const roundWoodTableUrl = "/images/furniture/round-wood-table.png";
+//furniture data
+const sofaData = {width: 300, height: 110, url: "/images/furniture/sofa.png"};
+const twinBedData = {width: 200, height: 350, url: "/images/furniture/twin-bed.png"};
+const singleBedData = {width: 170, height: 350, url: "/images/furniture/single-bed.png"};
+const loveseatData = {width: 200, height: 100, url: "/images/furniture/loveseat.png"};
+const plushChairData = {width: 100, height: 100, url: "/images/furniture/plush-chair.png"};
+const smallChairData = {width: 70, height: 70, url: "/images/furniture/small-chair.png"};
+const whiteChairData = {width: 70, height: 70, url: "/images/furniture/white-chair.png"};
+const smallWoodChairData = {width: 70, height: 70, url: "/images/furniture/small-wood-chair.png"};
+const tvStandData = {width: 140, height: 40, url: "/images/furniture/tv-stand.png"};
+const nightstandData = {width: 70, height: 70, url: "/images/furniture/nightstand.png"};
+const glassTableData = {width: 200, height: 100, url: "/images/furniture/glass-table.png"};
+const marbleCounterData = {width: 100, height: 300, url: "/images/furniture/marble-counter.png"};
+const fridgeData = {width: 120, height: 120, url: "/images/furniture/fridge.png"};
+const doubleFridgeData = {width: 200, height: 100, url: "/images/furniture/fridge2.png"};
+const workspaceData = {width: 200, height: 200, url: "/images/furniture/workspace.png"};
+const patioWithChairsData = {width: 200, height: 200, url: "/images/furniture/patio-table-with-chairs.png"};
+const diningRoomTableData = {width: 250, height: 200, url: "/images/furniture/dining-room-table.png"};
+const woodTableData = {width: 200, height: 100, url: "/images/furniture/wood-table.png"};
+const roundMarbleTableData = {radius: 50, url: "/images/furniture/round-marble-table.png"};
+const roundWoodTableData = {radius: 50, url: "/images/furniture/round-wood-table.png"};
+const roundGlassTableData = {radius: 50, url: "/images/furniture/round-glass-table.png"};
+const kitchenSinkData = {width: 120, height: 60, url: "/images/furniture/kitchen-sink.png"};
+const stoveData = {width: 120, height: 90, url: "/images/furniture/stove.png"};
+const electricStoveData = {width: 120, height: 90, url: "/images/furniture/electric-stove.png"};
+const bathData = {width: 80, height: 200, url: "/images/furniture/bath.png"};
+const pottedPlantData = {radius: 30, url: "/images/furniture/potted-plant.png"};
+const bigPottedPlantData = {radius: 60, url: "/images/furniture/potted-plant.png"};
 
 //floor
 addFloor(placeholderFlooringUrl);
-
 var mouse = Mouse.create(render.canvas),
         mouseConstraint = MouseConstraint.create(engine, {
             mouse: mouse,
@@ -155,16 +170,13 @@ var mouse = Mouse.create(render.canvas),
                 }
             }
         });
-
 Composite.add(engine.world, mouseConstraint);
-
 //sync mouse to render
 render.mouse = mouse;
-
 Runner.run(engine);
 Render.run(render);
 
-//Load wall and furniture textures and create their bodies
+//Load sample wall and furniture textures and create their bodies
 //set brief delay to allow floor to load both its texture and body first
 setTimeout(function(){
   //walls
@@ -173,22 +185,20 @@ setTimeout(function(){
   addWall(roomWidth, 0, 50, roomHeight*2, wallUrl);
   addWall(0, roomHeight, roomWidth*2, 50, wallUrl);
   //furniture
-  addRectFurniture(80, 50, 250, 100, loveseatUrl);
-  addRectFurniture(300, 50, 300, 110, sofaUrl);
-  addRectFurniture(1200, 200, 200, 350, bed1Url);
-  addRectFurniture(800, 200, 200, 350, bed2Url);
-  addRectFurniture(600, 600, 120, 30, tvStandUrl);
-  addRectFurniture(1000, 100, 70, 70, nightstandUrl);
-  addRectFurniture(1000, 600, 200, 200, workspaceUrl);
-  addRectFurniture(100, 600, 250, 200, diningRoomTableUrl);
-  addRoundFurniture(500, 300, 50, roundMarbleTableUrl);
-  addRoundFurniture(700, 300, 50, roundWoodTableUrl);
-}, 100);
-
-function selectFloor() {
-  let selectedFloor = document.getElementById("floor-picker").value;
-  floor.render.sprite.texture = `/images/floors/${selectedFloor}.png`
-}
+  addRectFurniture(100, 50, 250, 100, loveseatData.url);
+  addRectFurniture(300, 50, 300, 110, sofaData.url);
+  addRectFurniture(100, 300, 100, 100, plushChairData.url);
+  addRectFurniture(1200, 200, 200, 350, twinBedData.url);
+  addRectFurniture(800, 200, 170, 350, singleBedData.url);
+  addRectFurniture(600, 600, 120, 30, tvStandData.url);
+  addRectFurniture(1000, 100, 70, 70, nightstandData.url);
+  addRectFurniture(roomWidth - 100, roomHeight - 100, 200, 200, workspaceData.url);
+  addRectFurniture(100, roomHeight - 100, 250, 200, diningRoomTableData.url);
+  addRoundFurniture(500, 300, 50, roundMarbleTableData.url);
+  addRoundFurniture(700, 300, 50, roundWoodTableData.url);
+  addRoundFurniture(400, roomHeight - 100, 50, pottedPlantData.url);
+  addRoundFurniture(30, 120, 30, bigPottedPlantData.url);
+}, 150);
 
 //Capture body user is targeting
 Events.on(mouseConstraint, "startdrag", function(e){
@@ -230,9 +240,104 @@ window.addEventListener('keydown', (e) => {
     Matter.Body.rotate(targetFurniture, 0.07);
   });
 
-// window.addEventListener('resize', () => {
-//   roomWidth = window.innerWidth * 0.6;
-//   roomHeight = window.innerHeight * 0.8;
-//   render.width = roomWidth;
-//   render.height = roomHeight;
-// });
+function selectFloor() {
+  let selectedFloor = document.getElementById("floor-picker").value;
+  floor.render.sprite.texture = `/images/floors/${selectedFloor}.png`
+}
+
+function selectNewFurniture() {
+  let selectedFurniture = document.getElementById("furniture-picker").value;
+
+  let furnitureData;
+  switch (selectedFurniture) {
+    case ("sofa"):
+      furnitureData = sofaData;
+      break;
+    case ("loveseat"):
+      furnitureData = loveseatData;      
+      break;
+    case ("single-bed"):
+      furnitureData = singleBedData;      
+      break;
+    case ("twin-bed"):
+      furnitureData = twinBedData;      
+      break;
+    case ("small-chair"):
+      furnitureData = smallChairData;      
+      break;
+    case ("white-chair"):
+      furnitureData = whiteChairData;      
+      break;
+    case ("plush-chair"):
+      furnitureData = plushChairData;      
+      break;
+    case ("wood-chair"):
+      furnitureData = smallWoodChairData;      
+      break;
+    case ("nightstand"):
+      furnitureData = nightstandData;      
+      break;
+    case ("tv-stand"):
+      furnitureData = tvStandData;      
+      break;
+    case ("dining-room-table"):
+      furnitureData = diningRoomTableData;      
+      break;
+    case ("wood-table"):
+      furnitureData = woodTableData;      
+      break;
+    case ("workspace"):
+      furnitureData = workspaceData;      
+      break;
+    case ("marble-counter"):
+      furnitureData = marbleCounterData;      
+      break;
+    case ("round-glass-table"):
+      furnitureData = roundGlassTableData;      
+      break;
+    case ("round-wood-table"):
+      furnitureData = roundWoodTableData;      
+      break;
+    case ("round-marble-table"):
+      furnitureData = roundMarbleTableData;      
+      break;
+    case ("glass-table"):
+      furnitureData = glassTableData;      
+      break;
+    case ("kitchen-sink"):
+      furnitureData = kitchenSinkData;      
+      break;
+    case ("stove"):
+      furnitureData = stoveData;      
+      break;
+    case ("electric-stove"):
+      furnitureData = electricStoveData;      
+      break;
+    case ("double-fridge"):
+      furnitureData = doubleFridgeData;      
+      break;
+    case ("fridge"):
+      furnitureData = fridgeData;      
+      break;
+    case ("bath"):
+      furnitureData = bathData;      
+      break;
+    case ("patio-table-with-chairs"):
+      furnitureData = patioWithChairsData;      
+      break;
+    case ("potted-plant"):
+      furnitureData = pottedPlantData;      
+      break;
+    case ("big-potted-plant"):
+      furnitureData = bigPottedPlantData;      
+      break;
+    default:
+      furnitureData = loveseatData; 
+  }
+
+  if (furnitureData.radius)
+    addRoundFurniture(roomWidth/2, roomHeight/2, furnitureData.radius, furnitureData.url)
+  else
+    addRectFurniture(roomWidth/2, roomHeight/2, furnitureData.width, furnitureData.height, furnitureData.url);
+}
+
